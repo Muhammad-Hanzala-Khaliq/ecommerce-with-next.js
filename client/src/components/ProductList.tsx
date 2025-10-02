b7971ad77d0bd383babbe6a6b7a670aaad9cd8ee
M.Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2025-10-02 15:24:20 +0500

    feat: design ecommerce homepage, product listing, categories & interactive product cards with color-based image switching

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -1,4 +1,7 @@
 import { ProductsType} from "@/types"
+import Categories from "./Categories"
+import ProductCard from "./ProductCard"
+import Link from "next/link"
 
 //TEMPORARY
 const products:ProductsType = [
@@ -112,9 +115,17 @@ const products:ProductsType = [
   },
 ]
 
-const ProductList = () => {
+const ProductList = ({category}:{category:string}) => {
   return (
-    <div className="w-full">ProductList</div>
+    <div className="w-full">
+      <Categories/>
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
+      {products.map(product=> (
+        <ProductCard key={product.id} product={product}/>
+      ))}
+      </div>
+      <Link href={category ? `/products/?category=${category}`:"/products"} className="flex justify-end mt-4 underline text-sm text-gray-500">View All Products</Link>
+    </div>
   )
 }
 
