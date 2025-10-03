d6865bda6adc91af95f459602f5b5b932633836f
M.Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2025-10-03 13:54:13 +0500

    feat: add product list page, product filtering, cart with steps, and payment form with RHF + Zod validation

--- client/src/components/ProductList.tsx
+++ client/src/components/ProductList.tsx
@@ -2,9 +2,10 @@ import { ProductsType} from "@/types"
 import Categories from "./Categories"
 import ProductCard from "./ProductCard"
 import Link from "next/link"
+import Filter from "./Filter";
 
 //TEMPORARY
-const products:ProductsType = [
+const products: ProductsType = [
   {
     id: 1,
     name: "Adidas CoreFit T-Shirt",
@@ -113,20 +114,32 @@ const products:ProductsType = [
     colors: ["blue", "green"],
     images: { blue: "/products/8b.png", green: "/products/8gr.png" },
   },
-]
+];
 
-const ProductList = ({category}:{category:string}) => {
+const ProductList = ({
+  category,
+  params,
+}: {
+  category: string;
+  params: "homepage" | "products";
+}) => {
   return (
     <div className="w-full">
-      <Categories/>
+      <Categories />
+      {params === "products" && <Filter />}
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
-      {products.map(product=> (
-        <ProductCard key={product.id} product={product}/>
-      ))}
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
-      <Link href={category ? `/products/?category=${category}`:"/products"} className="flex justify-end mt-4 underline text-sm text-gray-500">View All Products</Link>
+      <Link
+        href={category ? `/products/?category=${category}` : "/products"}
+        className="flex justify-end mt-4 underline text-sm text-gray-500"
+      >
+        View All Products
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default ProductList
